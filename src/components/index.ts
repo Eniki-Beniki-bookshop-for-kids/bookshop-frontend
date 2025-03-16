963567da6d97a466cd1ddc62fb7054ed07516694
Oleh Kliapko <goldclub1977@gmail.com>
2025-03-16 14:51:51 +0200

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 import { AccountMenu } from "./AccountMenu"
-import { Container } from "./Container"
 import { Footer } from "./Footer"
 import { Header } from "./header/Header"
 
-export { AccountMenu, Container, Footer, Header }
+export { AccountMenu, Footer, Header }
