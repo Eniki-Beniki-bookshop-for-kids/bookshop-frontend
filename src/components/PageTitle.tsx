6bc5694c2ef3237f13c793c35ef6de3784a7096a
Oleh Kliapko <goldclub1977@gmail.com>
2025-04-28 20:58:00 +0300

    Update PageTitle.tsx

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,6 +1,12 @@
 import { GridItem, Heading } from "@chakra-ui/react"
 
-export const PageTitle = ({ title, mb = 9 }: { title: string; mb: number }) => {
+export const PageTitle = ({
+	title,
+	mb = 9,
+}: {
+	title: string
+	mb?: number
+}) => {
 	return (
 		<GridItem area="title">
 			<Heading as="h1" fontSize={28} fontWeight="400" mb={mb}>
